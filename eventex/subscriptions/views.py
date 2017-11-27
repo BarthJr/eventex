5eec8739f9cc41abd773cfdfa9e3a550621eaa10
Barth <juniior.barth@gmail.com>
2017-11-27 11:58:46 -0200

    Persiste a inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,6 +6,7 @@ from django.template.loader import render_to_string
 
 from django.conf import settings
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -31,6 +32,7 @@ def create(request):
                form.cleaned_data)
 
 
+    Subscription.objects.create(**form.cleaned_data)
     messages.success(request, 'Inscrição realizada com sucesso!')
 
     return HttpResponseRedirect('/inscricao')

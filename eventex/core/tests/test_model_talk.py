515cf945a6fd883b273d3dc481bd6476410dd24f
Barth <juniior.barth@gmail.com>
2017-12-13 22:27:00 -0200

    Course como MTI

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
 from eventex.core.managers import PeriodManager
-from eventex.core.models import Talk, Course
+from eventex.core.models import Talk, Course, Course
+
 
 class TalkModelTest(TestCase):
     def setUp(self):
@@ -40,6 +41,9 @@ class TalkModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
 
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
+
 class PeriodManagerTest(TestCase):
     def setUp(self):
         Talk.objects.create(title='Morning Talk', start='11:59')
@@ -57,6 +61,7 @@ class PeriodManagerTest(TestCase):
         qs = Talk.objects.at_afternoon()
         expected = ['Afternoon Talk']
         self.assertQuerysetEqual(qs, expected, lambda o: o.title)
+
 
 
 class CourseModelTest(TestCase):
@@ -85,3 +90,6 @@ class CourseModelTest(TestCase):
 
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
